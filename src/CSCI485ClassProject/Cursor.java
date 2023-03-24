1ce0c86a7db1ee0b1c090461a00e9f39d323cd2f
Josh Lee <joshlee@usc.edu>
2023-03-23 22:48:29 -0700

    Debug stuff

--- src/CSCI485ClassProject/Cursor.java
+++ src/CSCI485ClassProject/Cursor.java
@@ -83,7 +83,7 @@ public class Cursor {
       curr = iterator.next();
     }
     else return null;
-    currRecord =  keyvalueToFDBKVPair(getNextSetOfFDBKVPairs(keyvalueList));
+    currRecord = keyvalueToFDBKVPair(getNextSetOfFDBKVPairs(keyvalueList));
     return currRecord;
   }
 
@@ -100,7 +100,7 @@ public class Cursor {
       curr = iterator.next();
     }
     else return null;
-    currRecord =  keyvalueToFDBKVPair(getNextSetOfFDBKVPairs(keyvalueList));
+    currRecord = keyvalueToFDBKVPair(getNextSetOfFDBKVPairs(keyvalueList));
     return currRecord;
   }
 
@@ -126,7 +126,7 @@ public class Cursor {
 
   public StatusCode delete() {
     for (FDBKVPair kvpair : currRecord) {
-      System.out.println("attrname: " + kvpair.getKey().getString(metadata.getPrimaryKeys().size()+1) + " attrvalue: " + kvpair.getValue().get(0));
+      System.out.println("attrname: " + kvpair.getKey().getString(metadata.getPrimaryKeys().size()) + " attrvalue: " + kvpair.getValue().get(0));
       FDBHelper.removeKeyValuePair(tx, subspace, kvpair.getKey());
     }
     return StatusCode.SUCCESS;
